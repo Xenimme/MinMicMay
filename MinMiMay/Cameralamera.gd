ec6a628969dfb2acc54362b05b5343db99ae2d28
Xenimme <106463691+Xenimme@users.noreply.github.com>
2023-05-02 15:31:08 +0100

    Delete MinMiMay directory

--- MinMiMay/Cameralamera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if Input.is_action_just_released("Zoom Out"):
-		set_zoom(get_zoom() - Vector2(0.025, 0.025))
-	if Input.is_action_just_released("Zoom In"):
-		set_zoom(get_zoom() + Vector2(0.025, 0.025))
